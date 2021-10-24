dfe077843079e63015bbff541f75e6998922e823
jomurgel <dev@jomurgel.com>
2021-10-24 16:02:14 -0600

    resolves test issues

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,18 @@
-export const roots = ['<rootDir>'];
-export const testEnvironment = 'jsdom';
-export const moduleFileExtensions = ['ts', 'tsx', 'js', 'json', 'jsx'];
-export const testPathIgnorePatterns = ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'];
-export const transformIgnorePatterns = ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'];
-export const transform = {
-  '^.+\\.(ts|tsx)$': 'babel-jest',
-};
-export const watchPlugins = [
-  'jest-watch-typeahead/filename',
-  'jest-watch-typeahead/testname',
-];
-export const moduleNameMapper = {
-  '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-  '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+module.exports = {
+  roots: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
 };
