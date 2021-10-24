136c67642cfd7c038204091241d5606a9ab1e7e0
jomurgel <dev@jomurgel.com>
2021-10-24 14:19:13 -0600

    adds next-seo config

---
+++ next-seo-config.ts
@@ -0,0 +1,14 @@
+const nextConfig = {
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    site_name: 'jomurgel.com',
+  },
+  twitter: {
+    handle: '@jomurgel',
+    site: '@jomurgel',
+    cardType: 'summary_large_image',
+  },
+};
+
+export default nextConfig;
